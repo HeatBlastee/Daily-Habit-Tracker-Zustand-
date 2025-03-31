8789cb73a81aea38e3893f16d85fcd1144d3ed9e
HeatBlastee <mann42006@gmail.com>
2025-03-31 16:31:16 +0530

    Habit tracker completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { Box, Container, Typography } from "@mui/material";
+import AddHabitForm from "./components/add-habit-form";
+import HabitList from "./components/habit-list";
 
 
 function App() {
+
   
   return (
-    <>
-      
-
-    </>
+    <Container>
+      <Box>
+        <Typography variant="h2" component="h1" gutterBottom align="center" >
+          Habit Tracker
+        </Typography>
+        <AddHabitForm />
+        <HabitList/>
+      </Box>
+    </Container>
   )
 }
 
